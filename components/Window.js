ea424ea4bd0e9d9245c1f8492915ae36214774d1
jinah park <tbrk8585@naver.com>
2022-08-16 23:54:37 +0900

    react-window add & pre

--- components/Window.js
+++ components/Window.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { FixedSizeList as List } from "react-window";
 
-const Row = ({ index, style }) => <div style={style}>Row {index}</div>;
+const Row = ({ index, style }) => (
+    <div style={style} className={index % 2 ? "ListItemOdd" : "ListItemEven"}>
+        Row {index}
+    </div>
+);
 
-const Window = () => {
+const Window = (show) => {
     return (
         <>
-            <List height={150} itemCount={1000} itemSize={300} width={300}>
+            <List
+                height={150}
+                itemCount={1000}
+                itemSize={300}
+                width={300}
+                style={{
+                    width: "100%",
+                    height: show ? "80vh" : "100vh",
+                }}
+            >
                 {Row}
             </List>
         </>
--- pages/window.js
+++ pages/window.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Window from "../components/Window";
 import styles from "../styles/Home.module.css";
 
 const window = () => {
+    const [show, setShow] = useState(true);
     return (
-        <div className={styles.main}>
-            <Window />
+        <div className={styles.windowMain}>
+            {show && (
+                <div style={{ height: "5000px", width: "100%" }}>
+                    안녕하세요
+                    안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+                </div>
+            )}
+            <div
+                style={{
+                    top: 0,
+                    left: 0,
+                    height: "20vh",
+                    width: "100%",
+                }}
+            >
+                안녕하세요
+                <button onClick={(e) => setShow((cur) => !cur)}>
+                    클릭하세요!
+                </button>
+            </div>
+            <Window show={show} />
         </div>
     );
 };
